376ff409a834d7afa636e5f727a332a6aad2d471
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-08 23:08:37 +0530

    union find impl -2
    arr[i] indicates parent of i

---
+++ UnionFind/002_ParentUnionFind/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_ParentUnionFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * In the previous example, 
+                    Assumption: A and B objects are connected only if Arr[ A ] = Arr [ B ].
+                    But it has the following time complexity,
+                        Group => O(n^2)
+                        Find => O(1)
+            To bring one more improvement, instead of grouping them by changing the arr value to group name.
+            We can assume that arr[i] represent parent of i.
+            Suppose if arr[i] == i => it indicates the element is parent to itself. It can be called root.
+
+            Initially all the elements will be root as arr[i] == i.
+            To find the root, we will have while loop unless we find arr[i] == i
+
+            To find if two nodes are connected, we see if their root are same or not.
+
+            To connect element to another, we take root of first element and root of second element and connect the roots.
+            To connect the roots, r1, r2 we can say arr[r1] = r2 (or) arr[r2] = r1
+             */
+            PerformExample();
+        }
+        public static void PerformExample()
+        {
+            var unionFind = new UnionFind();
+            unionFind.Initialize(10);
+            unionFind.Group(2, 1);
+            unionFind.Group(4, 3);
+            unionFind.Group(8, 4);
+            unionFind.Group(9, 3);
+            unionFind.Group(6, 5);
+            PrintFind(9, 8, unionFind);
+            PrintFind(9, 0, unionFind);
+            PrintFind(6, 3, unionFind);
+            PrintFind(6, 5, unionFind);
+        }
+        public static void PrintFind(int a, int b, UnionFind unionFind)
+        {
+            Console.WriteLine($" Unioned? ({a}, {b}) : " + unionFind.Find(a, b));
+        }
+    }
+}
---
+++ UnionFind/002_ParentUnionFind/UnionFind.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_ParentUnionFind
+{
+    public class UnionFind
+    {
+        int[] arr;
+        public void Initialize(int n)
+        {
+            arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                // arr[i] represent the parent of i.
+                // arr[i] == i, this is root element. Initially all the elements are root.
+                arr[i] = i;
+            }
+        }
+
+        public int GetRoot(int a)
+        {
+            while (arr[a] != a)
+            {
+                a = arr[a];
+            }
+            return a;
+        }
+
+        public bool Find(int a, int b)
+        {
+            var r1 = GetRoot(a);
+            var r2 = GetRoot(b);
+            return r1 == r2;
+        }
+
+        public void Group(int a, int b)
+        {
+            var r1 = GetRoot(a);
+            var r2 = GetRoot(b);
+            arr[r1] = r2;
+        }
+    }
+}
