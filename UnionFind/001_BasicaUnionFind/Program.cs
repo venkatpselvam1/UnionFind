9fc11686bb1ef7e77f2bfdf09ad4995d54bf0413
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-08 22:53:12 +0530

    Union find basic impl - 1

---
+++ UnionFind/001_BasicaUnionFind/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_BasicaUnionFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+Example: You have a set of elements S = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}. Here you have 10 elements (N = 10 ).We can use an array Arr to manage the connectivity of elements. Arr[ ] indexed by elements of set, having size of N (as N elements in set) and can be used to manage the above operations.
+
+Assumption: A and B objects are connected only if Arr[ A ] = Arr [ B ].
+
+Now how we will implement above operations :
+
+Find (A, B) - check if Arr[ A ] is equal to Arr[ B ] or not. Union (A, B) - Connect A to B and merge the components having A and B by changing all the elements ,whose value is equal to Arr[ A ], to Arr[ B ].
+             */
+            PerformExample();
+        }
+        public static void PerformExample()
+        {
+            var unionFind = new UnionFind();
+            unionFind.Initialize(10);
+            unionFind.Group(2, 1);
+            unionFind.Group(4, 3);
+            unionFind.Group(8, 4);
+            unionFind.Group(9, 3);
+            unionFind.Group(6, 5);
+            PrintFind(9, 8, unionFind);
+            PrintFind(9, 0, unionFind);
+            PrintFind(6, 3, unionFind);
+            PrintFind(6, 5, unionFind);
+        }
+        public static void PrintFind(int a, int b, UnionFind unionFind)
+        {
+            Console.WriteLine($" Unioned? ({a}, {b}) : "+unionFind.Find(a, b));
+        }
+    }
+}
---
+++ UnionFind/001_BasicaUnionFind/UnionFind.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_BasicaUnionFind
+{
+    public class UnionFind
+    {
+        int[] arr;
+        public void Initialize(int n)
+        {
+            // arr[i] represent the group number of the element i.
+            // initially we have n group named 0,1,...n-1
+            arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = i;
+            }
+        }
+
+        public void Group(int a, int b)
+        {
+            // Group1 => Group which contains the element a
+            // Group2 => Group which contains the element b
+            // merge the two group => Group1 and Group2
+            // To merge we have two option,
+            // a) all the elements of the Group2 can be moved to Group1 (or)
+            // b) all the elements of the Group1 can be moved to Group2 (or)
+
+            // here I am changing all the group1 elements to group2
+            var group1 = arr[a];
+            var group2 = arr[b];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == group1)
+                {
+                    arr[i] = group2;
+                }
+            }
+
+            // To add 1 elements to group, time complexity is O(n)
+            // To add n elements to group, time complexity is O(n^2)
+        }
+        public bool Find(int a, int b)
+        {
+            return arr[a] == arr[b];
+            // To find if the two elements are in the same group, time complexity is O(1)
+        }
+    }
+}
