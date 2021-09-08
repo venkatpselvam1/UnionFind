52135915016fef3e2768a20e4dd35b259f028fb4
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-08 22:28:03 +0530

    adding new sln

---
+++ UnionFind/UnionFind/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * The efficiency of an algorithm sometimes depends on using an efficient data structure. A good choice of data structure can reduce the execution time of an algorithm and Union-Find is a data structure that falls in that category.
+
+                Let’s say, you have a set of N elements which are partitioned into further subsets, and you have to keep track of connectivity of each element in a particular subset or connectivity of subsets with each other. To do this operation efficiently, you can use Union-Find Data Structure.
+
+                Let’s say there are 5 people A, B, C, D E. A is a friend of B, B is a friend of C and D is a friend of E. As we can see:
+
+                1) A, B and C are connected to each other.
+                2) D and E are connected to each other.
+
+                So we can use Union Find Data Structure to check whether one friend is connected to another in a direct or indirect way or not. We can also determine the two different disconnected subsets. Here 2 different subsets are {A, B, C} and {D, E}.
+
+                You have to perform two operations here :
+
+                Union (A, B) - connect two elements A and B. Find (A, B) - find, is there any path connecting two elements A and B
+             */
+        }
+    }
+}
