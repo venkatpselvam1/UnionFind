cda67846717baa40c4f6aa13bd4216437ffa370f
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-09 12:37:47 +0530

    weighted union find

---
+++ UnionFind/003_ParentUnionFindMinTreeLength/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_ParentUnionFindMinTreeLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * In the previous example, 
+                    Assumption: A and B objects are connected only if root(A) == root(B).
+                    Arr[i] represent the parent of i.
+                    if arr[i] == i => it is root node
+                But,
+                    when Group two element(A and B), we have two choice,  
+                        a) we can change the parent of A to B (or)
+                        b) we can change the parent of B to A
+                    It lead to a worst case scenario like long stack.
+                        e.g. 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+
+                So the idea is, we can give preference to size of capacity.
+                if we want to have long list,           => we will join the long group to small group
+                if we want to have short list,          => we will join the small group to large
+
+                in this example, we want to keep the tree length as short as we can.
+             */
+            PerformExample();
+        }
+        public static void PerformExample()
+        {
+            var unionFind = new UnionFind();
+            unionFind.Initialize(6);
+            unionFind.Group(0, 1);
+            unionFind.Group(1, 2);
+            unionFind.Group(3, 2);
+            PrintFind(3, 0, unionFind);
+            PrintFind(4, 0, unionFind);
+        }
+        public static void PrintFind(int a, int b, UnionFind unionFind)
+        {
+            Console.WriteLine($" Unioned? ({a}, {b}) : " + unionFind.Find(a, b));
+        }
+    }
+}
---
+++ UnionFind/003_ParentUnionFindMinTreeLength/UnionFind.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_ParentUnionFindMinTreeLength
+{
+    public class UnionFind
+    {
+        int[] arr;
+        int[] weight;
+        public void Initialize(int n)
+        {
+            arr = new int[n];
+            weight = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = i;
+                weight[i] = 1;
+            }
+        }
+
+        public int FindRoot(int a)
+        {
+            while (arr[a] != a)
+            {
+                a = arr[a];
+            }
+
+            return a;
+        }
+
+        public void Group(int a, int b)
+        {
+            var r1 = FindRoot(a);
+            var r2 = FindRoot(b);
+            if (weight[r1] < weight[r2])
+            {
+                arr[r1] = r2;
+                weight[r2] += weight[r1];
+            }
+            else
+            {
+                arr[r2] = r1;
+                weight[r1] += weight[r2];
+            }
+        }
+
+        public bool Find(int a, int b)
+        {
+            var r1 = FindRoot(a);
+            var r2 = FindRoot(b);
+            return r1 == r2;
+        }
+    }
+}
