65ab6786da907ae119c9cba8ee46bdcced5e80f5
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-10 11:24:13 +0530

    Redundant Connection => finding cycles in graph using union-find-weighted

---
+++ UnionFind/002_Redundant_Connection_Weighted/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Redundant_Connection_Weighted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * For the same above problem, we are going to give one more improvement.
+             * When we want to group two element (A, B),
+             * we will get roots of these elements (r1, r2)
+             * We will check, which group have more capacity, we will add the small group to the other group.
+             * So that the execution will be faster.
+             */
+            var sln = new Solution();
+            var edges = new int[3][]
+            {
+                    new int[]{1,2},
+                    new int[]{1,3},
+                    new int[]{2,3}
+            };
+            var ans = sln.FindRedundantConnection(edges);
+            foreach (var item in ans)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+        public class Solution
+        {
+            public int[] FindRedundantConnection(int[][] edges)
+            {
+                var uf = new UnionFind();
+                for (int i = 0; i < edges.Length; i++)
+                {
+                    if (!uf.Union(edges[i][0], edges[i][1]))
+                    {
+                        return edges[i];
+                    }
+                }
+
+                return new int[0];
+            }
+        }
+    }
+}
---
+++ UnionFind/002_Redundant_Connection_Weighted/UnionFind.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Redundant_Connection_Weighted
+{
+    public class UnionFind
+    {
+        // for the any element i, dict[i] indicates the parent.
+        // The parent of two property, one is index(next parent) and another is the group capacity.
+        Dictionary<int, WeightedNode> dict = new Dictionary<int, WeightedNode>();
+
+        private WeightedNode GetRoot(int a)
+        {
+            // if the given element is not part of the node, then it root node.
+            // Add it to dict.
+            if (!dict.ContainsKey(a))
+            {
+                dict.Add(a, new WeightedNode(a));
+            }
+            while (dict[a].I != a)
+            {
+                a = dict[a].I;
+            }
+
+            return dict[a];
+        }
+
+        public bool Union(int a, int b)
+        {
+            var r1 = GetRoot(a);
+            var r2 = GetRoot(b);
+            if (r1.I == r2.I)
+            {
+                return false;
+            }
+
+            if (r1.C > r2.C)
+            {
+                r2.I = r1.I;
+                r1.C += r2.C;
+            }
+            else
+            {
+                r1.I = r2.I;
+                r2.C += r1.C;
+            }
+
+            return true;
+        }
+
+        private class WeightedNode
+        {
+            public WeightedNode(int i)
+            {
+                this.I = i;
+                this.C = 1;
+            }
+            // index of the parent
+            public int I;
+            // capacity of the parent
+            public int C;
+        }
+    }
+}
