8e70e5ee303194b0fff0860007586b697dcef0de
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-10 11:11:56 +0530

    finding cycles in graph

---
+++ UnionFind/001_Redundant_Connection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Redundant_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://leetcode.com/problems/redundant-connection/
+             * In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+             *You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added.
+             *The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed.
+             *The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+
+             * Return an edge that can be removed so that the resulting graph is a tree of n nodes.
+             * If there are multiple answers, return the answer that occurs last in the input.
+             */
+            var sln = new Solution();
+            var edges = new int[3][]
+            {
+                    new int[]{1,2},
+                    new int[]{1,3},
+                    new int[]{2,3}
+            };
+            var ans = sln.FindRedundantConnection(edges);
+            foreach (var item in ans)
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine();
+        }
+
+        public class Solution
+        {
+            public int[] FindRedundantConnection(int[][] edges)
+            {
+                var uf = new UnionFind();
+                for (int i = 0; i < edges.Length; i++)
+                {
+                    if (!uf.Union(edges[i][0], edges[i][1]))
+                    {
+                        return edges[i];
+                    }
+                }
+
+                return new int[0];
+            }
+        }
+    }
+}
---
+++ UnionFind/001_Redundant_Connection/UnionFind.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Redundant_Connection
+{
+    public class UnionFind
+    {
+        // for the node i, the parent node is dict[i]
+        // if a node don't present in the dict => it indicates it is root node.
+        // initiall dict don't have any entry. So  all the nodes are root node.
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        public int GetRoot(int a)
+        {
+            // get root node, root node is something which is not in the dictionary
+            while (dict.ContainsKey(a))
+            {
+                a = dict[a];
+            }
+
+            return a;
+        }
+        // The bool indicates if the children are already part of same group.
+        // Adding them again, will create circular dependency.
+        public bool Union(int a, int b)
+        {
+            var r1 = GetRoot(a);
+            var r2 = GetRoot(b);
+            if (r1 == r2)
+            {
+                return false;
+            }
+            // we are making r1's parent as r2
+            dict.Add(r1, r2);
+            return true;
+        }
+    }
+}
